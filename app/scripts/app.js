307bfb775f023900efa21c7f1fa3c49f659d3d89
kavalar <apuc06@mail.ru>
2018-01-26 14:51:16 +0300

    add responsive

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -3,4 +3,24 @@ import $ from 'jquery';
 
 $(() => {
 	svg4everybody();
+
+	$(document).on('click', '.nav__item a', function () {
+		event.preventDefault();
+		const id = $(this).attr('href');
+		$('html, body').animate({
+			scrollTop: $('section[id=' + id + ']').offset().top
+		}, 1000);
+		if (window.innerWidth < 992) {
+			$('ul.nav__col').slideUp('fast');
+			$('.nav__btn').removeClass('nav__btn--close');
+		}
+	});
+
+	$(document).on('click', '.nav__btn', function () {
+		if (window.innerWidth < 992) {
+			$(this).toggleClass('nav__btn--close');
+			$('ul.nav__col').slideToggle().css('display', 'flex');
+		}
+	});
+
 });
